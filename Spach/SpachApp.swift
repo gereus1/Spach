9bf989a90960d477b8434ecd0a1ecefb457a5b35
Andrew <av@starta.one>
2025-04-17 20:36:57 +0300

    Initial Commit

---
+++ Spach/SpachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpachApp.swift
+//  Spach
+//
+//  Created by Andrew Valivaha on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpachTests/SpachTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpachTests.swift
+//  SpachTests
+//
+//  Created by Andrew Valivaha on 17.04.2025.
+//
+
+import Testing
+
+struct SpachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
